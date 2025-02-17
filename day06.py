ad59dd8ab6ff8834d4c52b845c5f76de6008c740
lhe <gsurplus@inha.edu>
2025-02-17 14:05:38 +0900

    v0.7 day06 poly

--- day06.py
+++ day06.py
@@ -1,4 +1,30 @@
-# 0(1) 상수시간만 받음. 반복문의 횟수가 적어질수록 효율이 좋구나.
-# 상속을 안 할 수 없으나 최소한으로 사용하기.
-n = int(input())
-print(n * (n+1) // 2)
+def print_poly(f_x, t_x) -> str:
+    #term = len(f_x) - 1
+    poly_expression = "f(x) = "
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        term = t_x[i]
+
+        if coefficient >= 0:
+            poly_expression = poly_expression + "+"
+        poly_expression = poly_expression + f'{coefficient}x^{term} '
+    return poly_expression
+
+
+def calculation_poly(x_value, f_x, t_x) -> int:
+    return_value = 0
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        term = t_x[i]
+        return_value += coefficient * pow(x_value, term)
+    return return_value
+
+
+fx = [2, 5, -9, 11]
+tx = [20, 7, 2, 0]
+
+if __name__ == "__main__":
+    print(print_poly(fx, tx))
+    print(calculation_poly(int(input("x 값 : ")), fx, tx))
