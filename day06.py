9882cf5754b6a2089077a41bba7830714f85a0e3
lhe <gsurplus@inha.edu>
2025-02-17 11:36:18 +0900

    v0.5 day06 0(n)

--- day06.py
+++ day06.py
@@ -1,9 +1,7 @@
-def dec_oct(n) -> int:
-    if n == 0:
-        return ""
-    else:
-        return dec_oct(n // 8) + str(n % 8)
-
-
 n = int(input())
-print(dec_oct(n))
+
+sum = 0
+for i in range(n + 1):
+    sum = sum + i
+
+print(f"sum = {sum}")
