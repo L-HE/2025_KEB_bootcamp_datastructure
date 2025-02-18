7dfe007da15b5bc77b0186ea211d6478f0cf1558
lhe <gsurplus@inha.edu>
2025-02-18 10:08:43 +0900

    v1.5 day07 stack debugger

--- day07.py
+++ day07.py
@@ -1,28 +1,18 @@
-#상속 받는 것 아니면 () 생략, class 이름은 대문자로 시작.
-class Stack:
-    def __init__(self):
-        self.items = list()
-
-    def push(self, data):
-        self.items.append(data)
-
-    def pop(self) -> object:
-        return self.items.pop()
-
-    def size(self) -> int:
-        return len(self.items)
-
-    def peek(self) -> object:
-        return self.items[-1]
-
-    def is_empty(self) -> bool:
-        return len(self.items) == 0
+def a(i):
+    j = 9
+    print(i, j)
 
 
-s1 = Stack()
-s1.push(-9)
-s1.push(1)
-s1.push(977)
-print(s1.pop())
-print(s1.peek())
-print(s1.peek())
+def b(k):
+    a(1)
+    print(k)
+
+
+def main():
+    print("Start")
+    b(3)
+    print("end")
+
+
+if __name__ == "__main__":
+    main()
