e4deaf12f0976cf948fc4f950a925b1992783234
lhe <gsurplus@inha.edu>
2025-02-19 16:45:22 +0900

    v2.7 day08 Assignment BST

--- day08.py
+++ day08.py
@@ -25,20 +25,16 @@ def post_order(node):
     print(node.data, end=' ')
 
 
-class TreeNode:
-	def __init__(self):
-		self.left = None
-		self.data = None
-		self.right = None
+def inis_group(groups):
+    global root
 
-
-if __name__ == "__main__":
-    groups = ['블랙 핑크', '레드벨벳', '마마무', '에이핑크', '걸스데이','트와이스']
-    root = None
+    node = TreeNode()
+    node.data = groups[0]
+    root = node
 
     for group in groups[1:]:
         node = TreeNode()
-        node.data = groups[]
+        node.data = group
         current = root
         while True:
             if group < current.data:
@@ -51,23 +47,114 @@ if __name__ == "__main__":
                     current.right = node
                     break
                 current = current.right
-
     print("BST 구성 완료")
 
-    find_group = input()
 
+def insert_group(groups):
+    for group in groups[::]:
+        node = TreeNode()
+        node.data = group
+        current = root
+        while True:
+            if group < current.data:
+                if current.left is None:
+                    current.left = node
+                    break
+                current = current.left
+            else:
+                if current.right is None:
+                    current.right = node
+                    break
+                current = current.right
+        print(f"{group} 추가 완료")
+
+
+def find_group(group):
     current = root
     while True:
-        if find_group == current.data:
-            print(f"{find_group}을/를 찾았습니다.")
+        if group == current.data:
+            print(f"{group}을/를 찾았습니다.")
             break
-        elif find_group < current.data:
+        elif group < current.data:
             if current.left is None:
-                print(f"{find_group}이/가 존재하지 않습니다.")
+                print(f"{group}이/가 존재하지 않습니다.")
                 break
             current = current.left
         else:
             if current.right is None:
-                print(f"{find_group}이/가 존재하지 않습니다.")
+                print(f"{group}이/가 존재하지 않습니다.")
                 break
             current = current.right
+
+
+def delete_group(group):
+    current = root
+    parent = None
+    while True:
+        if group == current.data:
+            if current.left == None and current.right == None:
+                if parent.left == current:
+                    parent.left = None
+                else:
+                    parent.right = None
+                del current
+
+            elif current.left != None and current.right == None:
+                if parent.left == current:
+                    parent.left = current.left
+                else:
+                    parent.right = current.left
+                del current
+
+            elif current.left == None and current.right != None:
+                if parent.left == current:
+                    parent.left = current.right
+                else:
+                    parent.right = current.right
+                del current
+
+            print(f"{group}'이/가 삭제됨.")
+            break
+
+        elif group < current.data:
+            if current.left is None:
+                print(f"{group}이/가 존재하지 않습니다.")
+                break
+            parent = current
+            current = current.left
+        else:
+            if current.right is None:
+                print(f"{group}이/가 존재하지 않습니다.")
+                break
+            parent = current
+            current = current.right
+
+
+class TreeNode:
+
+    def __init__(self):
+        self.left = None
+        self.data = None
+        self.right = None
+
+
+if __name__ == "__main__":
+    groups = input("그룹을 입력하세요: ").split(' ')
+    inis_group(groups)
+    while True:
+        num = int(input("번호를 입력하세요.\n1) 삽입   2) 검색   3) 삭제   4) 종료   : "))
+        if num == 1:
+            i_group = input("삽입할 그룹을 입력하세요: ").split(' ')
+            insert_group(i_group)
+        elif num == 2:
+            f_group = input("검색할 그룹을 입력하세요: ")
+            find_group(f_group)
+        elif num == 3:
+            d_group = input("삭제할 그룹을 입력하세요: ")
+            delete_group(d_group)
+        elif num == 4:
+            print("프로그램 종료")
+            break
+        else:
+            print("다시 입력해주세요.")
+
