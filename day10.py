f047afc153fff2fb14342e7004538f9d9fe01599
lhe <gsurplus@inha.edu>
2025-02-21 13:18:07 +0900

    v3.5 day10 insertion vs bubble

--- day10.py
+++ day10.py
@@ -1,14 +1,35 @@
+import time
+import random
+import copy
+
+
+def time_decorator(func):
+    """
+    td
+    """
+    def wrapper(*arg):
+        """
+        wrapper
+        """
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'duration time: {e - s}sec')
+        return r
+    return  wrapper
+
+@time_decorator
 def insertion_sort(list):
     for i in range(1, len(list)):
         value = list[i]
         while i > 0 and list[i - 1] > value:
             list[i] = list[i - 1]
             i -= 1
-            print(i, end=' ')
+            #print(i, end=' ')
         list[i] = value
     return list
 
-
+@time_decorator
 def bubble_sort(list):
     count = 0
     for i in range(len(list) - 1):
@@ -18,11 +39,13 @@ def bubble_sort(list):
                 list[j], list[j + 1] = list[j + 1], list[j]
                 no_swaps = False
             count += 1
-            print(j, end=' ')
+            #print(j, end=' ')
         if no_swaps:
             return list
     print(f"\n{count}")
 
 
-print(bubble_sort([8, -11, 91, 35, 10])
-print(insertion_sort([8, -11, 91, 35, 10]))
+lists1 = [random.randint(1, 100000) for _ in range(10000)]
+lists2 = lists1.copy()
+print(insertion_sort(lists1))
+print(bubble_sort(lists2))
