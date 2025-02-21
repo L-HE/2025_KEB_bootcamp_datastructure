b15670c05ecfa0742eed9b68b91534374a4e9b74
lhe <gsurplus@inha.edu>
2025-02-21 11:23:26 +0900

    v3.4 day10 insertion sort

--- day10.py
+++ day10.py
@@ -1,8 +1,28 @@
-def bubble_sort(list):
-    for i in range(len(list) - 1):
-        for j in range(len(list) - 1):
-            if list[j] > list[j + 1]:
-                list[j], list[j + 1] = list[j + 1], list[j]
+def insertion_sort(list):
+    for i in range(1, len(list)):
+        value = list[i]
+        while i > 0 and list[i - 1] > value:
+            list[i] = list[i - 1]
+            i -= 1
+            print(i, end=' ')
+        list[i] = value
     return list
 
-print(bubble_sort([8, -11, 9, 35, 10]))
+
+def bubble_sort(list):
+    count = 0
+    for i in range(len(list) - 1):
+        no_swaps = True
+        for j in range(len(list) - 1 - i):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+                no_swaps = False
+            count += 1
+            print(j, end=' ')
+        if no_swaps:
+            return list
+    print(f"\n{count}")
+
+
+print(bubble_sort([8, -11, 91, 35, 10])
+print(insertion_sort([8, -11, 91, 35, 10]))
