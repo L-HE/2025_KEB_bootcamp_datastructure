cfee299f765460dc64ad8477f93631fe69cf170f
lhe <gsurplus@inha.edu>
2025-03-28 19:50:41 +0900

    v3.9 250328 test2

--- 250328.py
+++ 250328.py
@@ -1,15 +1,9 @@
-t, k, n = map(int, input().split())
+n, m = map(int, input().split())
+basket = [0 for _ in range(n)]
 
-for _ in range(t):
-    down_layer = [i for i in range(1, n+1)]
-    up_layer = []
-    people = 0
-    for _ in range(k):
-        for j in range(n):
-            people += down_layer[j]
-            up_layer.insert(j, people)
-            if len(up_layer) > n:
-                up_layer.pop(-1)
-            down_layer[j] = up_layer[j]
-        people = 0
-    print(up_layer[-1])
+for _ in range(m):
+    i, j, k = map(int, input().split())
+    for a in range(i-1, j):
+        basket[a] = k
+
+print(basket)
